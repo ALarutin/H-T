4787cd2f489531b4afd879287855efab1d4cb287
mac <a.larutin@yandex.ru>
2019-03-04 20:19:55 +0300

    Added new in cal

--- go/1/calc/calc.go
+++ go/1/calc/calc.go
@@ -1,9 +1,49 @@
 package main
 
+import (
+	"errors"
+	"fmt"
+)
+
 // сюда писать код
 // фукция main тоже будет тут
 
+type Stack []int
 
-func main(){
+func main() {
+	var s string = "3 3 +"
+	val, err := Cal(s)
+	if err == nil {
+		fmt.Print(val)
+	} else {
+		fmt.Print(err)
+	}
+}
 
+func Cal(str string) (int, error) {
+	var stack Stack
+	for _, symbol := range str {
+		switch symbol {
+		case '\n':
+		case ' ':
+		case '+':
+		case '-':
+		case '*':
+		case '/':
+		}
+	}
+}
+
+func (stack *Stack) Pop() (int, error) {
+	length := len(*stack)
+	if length > 0 {
+		value := (*stack)[length-1]
+		*stack = (*stack)[:length-1]
+		return value, nil
+	}
+	return 0, errors.New("Stack is empty")
+}
+
+func (stack *Stack) Push(value int) {
+	*stack = append(*stack, value)
 }
