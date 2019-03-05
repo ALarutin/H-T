f6395cbde20a68a02ae6be3f757cff3ad3487208
mac <a.larutin@yandex.ru>
2019-03-05 23:46:05 +0300

    Finished calc

--- go/1/calc/calc.go
+++ go/1/calc/calc.go
@@ -1,18 +1,16 @@
 package main
 
 import (
+	"bufio"
 	"errors"
 	"fmt"
+	"os"
 )
 
-// сюда писать код
-// фукция main тоже будет тут
-
-type Stack []int
-
 func main() {
-	var s string = "3 3 +"
-	val, err := Cal(s)
+	reader := bufio.NewReader(os.Stdin)
+	str, _ := reader.ReadString('\n')
+	val, err := Calc(str)
 	if err == nil {
 		fmt.Print(val)
 	} else {
@@ -20,28 +18,114 @@ func main() {
 	}
 }
 
-func Cal(str string) (int, error) {
+func Calc(str string) (int, error) {
 	var stack Stack
-	for _, symbol := range str {
-		switch symbol {
-		case '\n':
-		case ' ':
-		case '+':
-		case '-':
-		case '*':
-		case '/':
+	var result = 0
+	var err error
+	var isItNumber bool
+	Loop:
+		for _, symbol := range str {
+			switch symbol {
+			case ' ', '=', '\n':
+				isItNumber = false
+			case '0', '1', '2', '3', '4', '5', '6', '7', '8', '9':
+				tempVal1 := int(symbol) - 48
+				if isItNumber {
+					tempVal2, tempErr := stack.Pop()
+					if tempErr != nil {
+						err = tempErr
+						break Loop
+					}
+					stack.Push(tempVal2 * 10 + tempVal1)
+
+				} else {
+					isItNumber = true
+					stack.Push(tempVal1)
+				}
+			case '+':
+				isItNumber = false
+				tempVal1, tempErr1 := stack.Pop()
+				tempVal2, tempErr2 := stack.Pop()
+				if tempErr1 != nil {
+					err = tempErr1
+					break Loop
+				}
+				if tempErr2 != nil{
+					err = tempErr2
+					break Loop
+				}
+				stack.Push(tempVal2 + tempVal1)
+			case '-':
+				isItNumber = false
+				tempVal1, tempErr1 := stack.Pop()
+				tempVal2, tempErr2 := stack.Pop()
+				if tempErr1 != nil {
+					err = tempErr1
+					break Loop
+				}
+				if tempErr2 != nil{
+					err = tempErr2
+					break Loop
+				}
+				stack.Push(tempVal2 - tempVal1)
+			case '*':
+				isItNumber = false
+				tempVal1, tempErr1 := stack.Pop()
+				tempVal2, tempErr2 := stack.Pop()
+				if tempErr1 != nil {
+					err = tempErr1
+					break Loop
+				}
+				if tempErr2 != nil{
+					err = tempErr2
+					break Loop
+				}
+				stack.Push(tempVal1 * tempVal2)
+			case '/':
+				isItNumber = false
+				tempVal1, tempErr1 := stack.Pop()
+				tempVal2, tempErr2 := stack.Pop()
+				if tempErr1 != nil {
+					err = tempErr1
+					break Loop
+				}
+				if tempErr2 != nil{
+					err = tempErr2
+					break Loop
+				}
+				if tempVal1 == 0{
+					err = errors.New("error: division by zero")
+					break Loop
+				}
+				stack.Push(tempVal2 / tempVal1)
+			case '.':
+				if isItNumber{
+					err = errors.New("error: float value")
+				} else{
+					err = errors.New("error: invalid symbol")
+				}
+				break Loop
+			default:
+				err = errors.New("error: invalid symbol")
+				break Loop
+			}
 		}
+	if err == nil{
+		result, err = stack.Pop()
 	}
+	return result, err
 }
+
+type Stack []int
 
 func (stack *Stack) Pop() (int, error) {
 	length := len(*stack)
-	if length > 0 {
-		value := (*stack)[length-1]
-		*stack = (*stack)[:length-1]
-		return value, nil
+	if length == 0 {
+		return 0, errors.New("error: stack is empty")
 	}
-	return 0, errors.New("Stack is empty")
+	value := (*stack)[length-1]
+	*stack = (*stack)[:length-1]
+	return value, nil
 }
 
 func (stack *Stack) Push(value int) {
--- go/1/calc/calc_test.go
+++ go/1/calc/calc_test.go
@@ -1,3 +1,60 @@
 package main
 
-// сюда писать тесты
+import (
+	"fmt"
+	"testing"
+)
+
+type successfulTestPair struct{
+	number int // for comfortable counting tests and understanding of what test you thinking about now
+	input string
+	result int
+	err string
+}
+
+var successfulTests = []successfulTestPair{
+	{number: 1, input: "1 1 +", result: 2,},
+	{number: 2, input: "1  1   +", result: 2,},
+	{number: 3, input: "3 2 -", result: 1,},
+	{number: 4, input: "2 3 *", result: 6,},
+	{number: 5, input: "8 4 /", result: 2,},
+	{number: 6, input: "2 4 -", result: -2,},
+	{number: 7, input: "2 3 /", result: 0,},
+	{number: 8, input: "2 5 * =", result: 10,},
+	{number: 9, input: "4 38 4 5 6 * + - /", result: 1,},
+	{number: 10, input: "100 99 - 2 * 38 + 2 3 / +", result: 40,},
+}
+
+func TestCalcSuccessful(t *testing.T) {
+	for i, item := range successfulTests {
+		result, err := Calc(item.input)
+		if err != nil {
+			fmt.Println(i)
+			t.Errorf("test %d, returned message: '%v'", i + 1 , err.Error())
+		}
+		if result != item.result {
+			t.Errorf("test %d, returned wrong result: %d, correct answer: %d", i + 1 , result ,item.result, )
+		}
+	}
+}
+
+var unsuccessfulTests = []successfulTestPair{
+	{number: 1, input: "1 +", err: "error: stack is empty",},
+	{number: 2, input: "3 0 /", err: "error: division by zero",},
+	{number: 3, input: "2.3 3 !", err: "error: float value",},
+	{number: 4, input: "a 4 -", err: "error: invalid symbol",},
+	{number: 5, input: "2 . 4", err: "error: invalid symbol",},
+	{number: 6, input: "error", err: "error: invalid symbol",},
+}
+
+func TestCalcUnsuccessful(t *testing.T) {
+	for i, item := range unsuccessfulTests {
+		_, err := Calc(item.input)
+		if err == nil{
+			t.Errorf("test %d, didn't return error, correct message: '%v'", i + 1 , item.err)
+		}
+		if err.Error() != item.err {
+			t.Errorf("test %d, returned wrong message: '%v', correct message: '%v'", i + 1, err.Error(), item.err)
+		}
+	}
+}
